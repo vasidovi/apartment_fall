c7e4852ae2822892716eb35d8286fe60b4c9823f
vasidovi <46259699+vasidovi@users.noreply.github.com>
2019-06-25 19:25:48 +0300

    initial commit

---
+++ apartment_fall/gameData.js
@@ -0,0 +1,12 @@
+const gameData  = {
+    main: {
+        rows: 7,
+        cols: 7,
+        gridfactor: 20,
+        margin: 5,        
+    }, 
+    interval: 20,
+    colors: {
+       background: "black", 
+    }
+}
