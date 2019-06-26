c094d91a8a8ed52fc447632db0b4634528ff20b8
Mindaugas Vasiliauskas <mivasiliauskas@gmail.com>
2019-06-26 16:26:05 +0300

    Added gitignore

--- setup.js
+++ setup.js
@@ -15,6 +15,19 @@ canvas.height = document.body.clientHeight;
 
 var pc = getPixelCanvas(canvas, 3, getInitialState());
 
+function play() {
+  /**
+   * If an AudioContext is created prior to the document receiving a
+   * user gesture, it will be created in the "suspended" state, and
+   * you will need to call resume() after a user gesture is
+   * received.
+   */
+    audioContext.resume().then(() => {
+        sound.play();
+    });
+}
+
+
 function getInitialState() {
   var state = copy(initialState);
 
